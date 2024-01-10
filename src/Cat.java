cba56b6c498ab1df36a9ab7b46ea9ec6d3412fcc
rabia <m1ny00n7177@gmail.com>
2024-01-10 22:10:52 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,29 @@
+public class Cat {
+    private int age;
+    private String name;
+    private String color;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Fish fish = new Fish();
+        fish.setName("Pet");
+        fish.setAge(5);
+        fish.setColor("Gold");
+
+        Parrot parrot = new Parrot();
+        parrot.setName("Darling");
+        parrot.setAge(12);
+        parrot.setColor("Blue");
+
+        Cat cat = new Cat();
+        cat.setName("Naen");
+        cat.setAge(10);
+        cat.setColor("Black");
+
+        Dog dog = new Dog();
+        dog.setName("Mingi");
+        dog.setAge(13);
+        dog.setColor("Wite");
+
+
+        System.out.println("Information about the fish:\n" + "Name: " + fish.getName() + "\n" + "Age: " + fish.getAge() + "\n" + "Color: " + fish.getColor() + "\n");
+        System.out.println("Information about the parrot:\n" + "Name: " + parrot.getName() + "\n" + "Age: " + parrot.getAge() + "\n" + "Color: " + parrot.getColor() + "\n");
+        System.out.println("Information about the cat:\n" + "Name: " + cat.getName() + "\n" + "Age: " + cat.getAge() + "\n" + "Color: " + cat.getColor() + "\n");
+        System.out.println("Information about the dog:\n" + "Name: " + dog.getName() + "\n" + "Age: " + dog.getAge() + "\n" + "Color: " + dog.getColor());
+
+
+    }
+}
